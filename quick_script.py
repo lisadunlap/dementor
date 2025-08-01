ca26ddfd2e05360d38568772cdcb65a0c0deba1e
Ethan Liu <87835921+ethantsliu@users.noreply.github.com>
2025-08-01 14:59:43 -0700

    Delete quick_script.py

--- quick_script.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("disguising/model-responses/base_500_all_models_2/google_gemma-3-27b-it.csv")
-df['prompt'].to_csv("data/gemma_prompts.txt", index=False, header=False)
